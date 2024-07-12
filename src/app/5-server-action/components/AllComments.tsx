a3e9b04a8aefda8ab9ce415e6ae70927f1fb62c7
Bonnie Schulkin <biz@bonnieschulkin.com>
2024-07-12 09:04:45 -0700

    Make separate pages for suspense page / component

---
+++ src/app/5-server-action/components/AllComments.tsx
@@ -0,0 +1,24 @@
+export const dynamic = "force-dynamic";
+import React from "react";
+
+import { getAllCommentsSlow } from "@/helpers/file-helpers";
+import styles from "@/styles/AllComments.module.css";
+import { Comment as CommentType } from "@/types";
+
+import Comment from "./Comment";
+
+export interface AllCommentsProps {}
+
+async function AllComments({}: AllCommentsProps) {
+  const comments = await getAllCommentsSlow();
+
+  return (
+    <div className={styles.wrapper}>
+      {comments.map((comment: CommentType) => (
+        <Comment key={comment.id} comment={comment} />
+      ))}
+    </div>
+  );
+}
+
+export default AllComments;
